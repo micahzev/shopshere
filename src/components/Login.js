08d3f1b6141519459cedd1a6facba070a788dcd2
micahzev <friedland.micah@gmail.com>
2017-09-29 18:30:47 -0500

    add login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,13 +4,21 @@ import { Link } from 'react-router';
 import { bindActionCreators } from 'redux';
 import { Input, ButtonInput, Modal, Button, DropdownButton, MenuItem, Grid, Row, Col, Alert } from 'react-bootstrap';
 import '~/node_modules/bootstrap/dist/css/bootstrap.css';
-import { routerMiddleware, push } from 'react-router-redux'
+import { routerMiddleware, push } from 'react-router-redux';
+
+import '~/src/styles/login.css';
+
 
 class Login extends Component {
 
   login() {
+    let email = document.getElementById('email').value;
     let secret = document.getElementById('password').value;
     window.localStorage.setItem('secretKey', secret);
+    window.localStorage.setItem('username', email);
+
+    alert(email+" " + secret);
+
     this.props.history.push('/');
   }
 
@@ -20,10 +28,17 @@ class Login extends Component {
 
   render() {
     return (
-      <div className="login-form" style= {{ position: 'absolute', left: 'calc(50% - 125px)', top: '150px' }}>
-        <label>Login:</label>
-        <input id='password'></input>
-        <Button onClick={this.login.bind(this)}>Go!</Button>
+      <div className="login-form">
+        <h1>Login to ShopSure Admin</h1>
+        <div className="input-form">
+        <label className="label-form">Email:</label>
+        <input className="text-input" id='email' ></input>
+        </div>
+        <div className="input-form">
+        <label className="label-form">Password:</label>
+        <input className="text-input" id='password'></input>
+        </div>
+        <Button className="login-button" onClick={this.login.bind(this)}>Go!</Button>
       </div>
     )
   }
